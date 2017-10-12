ba53f64caa7d631003dd9835082fa9df416814d7
Budi Irawan <budi.irawan@invoice2go.com>
2017-10-12 10:17:19 +0700

    feat: hero editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div><label>name: </label><input [(ngModel)]="hero.name" placeholder="name" /></div>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
+
+export class Hero {
+  id: number;
+  name: string;
 }
