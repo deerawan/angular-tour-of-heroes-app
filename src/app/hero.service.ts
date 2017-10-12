36ddc7e5764113b2fa3c638b8868081de639df18
Budi Irawan <budi.irawan@invoice2go.com>
2017-10-12 12:34:19 +0700

    feat: hero service

---
+++ src/app/hero.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { HEROES } from './mock-heroes';
+import { Hero } from './hero';
+
+@Injectable()
+export class HeroService {
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
+  }
+}
